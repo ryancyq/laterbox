17336e074a82ed352e15042dcd6d765ad6186775
Ryan Chang <ryancyq@gmail.com>
2023-12-14 01:11:43 +0800

    remove unused require statement

--- lib/staged_box.rb
+++ lib/staged_box.rb
@@ -2,7 +2,6 @@
 
 require_relative "staged_box/command"
 require_relative "staged_box/bootloader"
-require_relative "staged_box/boxer"
 require_relative "staged_box/config"
 require_relative "staged_box/version"
 
