34ffca7efc299cc316317b2867c4d758ef0b0dce
Ryan Chang <ryancyq@gmail.com>
2023-12-14 01:03:48 +0800

    merge default config with config file and cmd options and pass to bootloader (#17)

--- lib/staged_box.rb
+++ lib/staged_box.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
+require_relative "staged_box/command"
+require_relative "staged_box/bootloader"
+require_relative "staged_box/boxer"
+require_relative "staged_box/config"
 require_relative "staged_box/version"
 
 module StagedBox
   class Error < StandardError; end
-  # Your code goes here...
 end
---
+++ lib/staged_box/config.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require "yaml"
+
+module StagedBox
+  class Config
+
+    DEFAULT_LOG_LEVEL = 'info'.freeze
+    DEFAULT_BOXES = [].freeze
+    DEFAULT_CONCURRENCY = 3
+    DEFAULT_BUFFER_SIZE = 1
+
+    attr_accessor :default_log_level, :boxes, :concurreny, :buffer_size
+
+    def initialize(options = {})
+      set_defaults
+      @default_log_level = options[:default_log_level] if options.key?(:default_log_level)
+      @boxes             = options[:boxes] if options.key?(:boxes)
+      @concurreny        = options[:concurreny] if options.key?(:concurreny)
+      @buffer_size       = options[:buffer_size] if options.key?(:buffer_size)
+    end
+
+    def to_hash
+      {
+        default_log_level: @default_log_level,
+        boxes: @boxes,
+        concurreny: @concurrency,
+        buffer_size: @buffer_size
+      }
+    end
+
+    def self.from_file(file_path)
+      raise ArgumentError.new("No such config file #{file_path}") unless File.exist?(file_path)
+
+      file_content = File.read(file_path)
+      options = YAML.safe_load(file_content, permitted_classes: [Symbol], aliases: true) || {}
+      
+      if options.respond_to?(:deep_symbolize_keys!)
+        options.deep_symbolize_keys!
+      else
+        symbolize_keys_deep!(options)
+      end
+
+      new(options)
+    end
+
+  private
+
+    def set_defaults
+      @default_log_level = DEFAULT_LOG_LEVEL
+      @boxes             = DEFAULT_BOXES
+      @concurreny        = DEFAULT_CONCURRENCY
+      @buffer_size       = DEFAULT_BUFFER_SIZE
+    end
+
+    def symbolize_keys_deep!(hash)
+      hash.keys.each do |key|
+        symkey = key.respond_to?(:to_sym) ? key.to_sym : key
+        hash[symkey] = hash.delete(key)
+        symbolize_keys_deep!(hash[symkey]) if hash[symkey].is_a? Hash
+      end
+    end
+  end
+end
